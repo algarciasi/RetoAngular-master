e4962f8ca155909cbec164c7317fd94cd033b343
Alberto Miguel Garcia Simo <albertomiguelgarciasimo@outlook.com>
2025-04-10 19:37:31 +0200

    v1 cajitas Listado Empresas

--- src/app/pages/empresas-list/empresas-list.component.ts
+++ src/app/pages/empresas-list/empresas-list.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { EmpresaService } from '../../services/empresa.service';
 import { Empresa } from '../../interface/empresa';
 import { EmpresaCardComponent } from '../../components/empresa-card/empresa-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-empresas-list',
   standalone: true,
-  imports: [EmpresaCardComponent],
+  imports: [CommonModule, EmpresaCardComponent],
   templateUrl: './empresas-list.component.html',
   styleUrl: './empresas-list.component.css'
 })
