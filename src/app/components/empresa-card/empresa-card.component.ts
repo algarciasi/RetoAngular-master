7dae5ab456e8f63f1c260dfb17ddef6c2caee0f2
Alberto Miguel Garcia Simo <albertomiguelgarciasimo@outlook.com>
2025-04-10 19:56:16 +0200

    botones editar y eliminar listado empresas (no operativo aun)

--- src/app/components/empresa-card/empresa-card.component.ts
+++ src/app/components/empresa-card/empresa-card.component.ts
@@ -1,14 +1,30 @@
 import { Component, Input } from '@angular/core';
 import { Empresa } from '../../interface/empresa';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-empresa-card',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './empresa-card.component.html',
   styleUrl: './empresa-card.component.css'
 })
 export class EmpresaCardComponent {
 
   @Input() miEmpresa!: Empresa;
+
+  constructor(private router: Router) {}
+
+   editarEmpresa() {
+    this.router.navigate([`/empresas/editar/${this.miEmpresa.idEmpresa}`]);
+  }
+
+  eliminarEmpresa() {
+    const confirmar = confirm(`¿Seguro que deseas eliminar a ${this.miEmpresa.nombre}?`);
+    if (confirmar) {
+      this.router.navigate([`/empresas/eliminar/${this.miEmpresa.idEmpresa}`]);
+    }
+  }
 
 }
